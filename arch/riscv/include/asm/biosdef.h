a799068138cd67d002bee8d410f88970bb6fbeaf
test_admin <test_admin@163.com>
2025-09-24 09:55:44 +0800

    Init: P1 start

---
+++ arch/riscv/include/asm/biosdef.h
@@ -0,0 +1,10 @@
+#ifndef _ASM_BIOS_DEF_H
+#define _ASM_BIOS_DEF_H
+
+#define BIOS_PUTCHAR 1
+#define BIOS_GETCHAR 2
+#define BIOS_PUTSTR 9
+#define BIOS_SDWRITE 10
+#define BIOS_SDREAD 11
+
+#endif // _ASM_BIOS_DEF_H
